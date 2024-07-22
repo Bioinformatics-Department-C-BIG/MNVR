2d82849458ef9540d189f165aa684ea8e463e381
mariostomazou85 <mariost@cing.ac.cy>
2024-07-22 14:57:46 +0300

    Update function

---
+++ R/MNVR_pipeline.R
@@ -0,0 +1,78 @@
+MNVR_pipeline<-function(vcfdir=NULL,bamdir=NULL,whpath='~/.local/bin/whatshap',bcftpath='/usr/bin/bcftools',outpath=NULL,HGNC='/home/konstantinosp/tools/hgnc_complete_set.txt',gffpath=NULL,reference=NULL,hgnc=NULL){
+
+  WHpath<-whpath
+  BCFtpath<-bcftpath
+  # outpath<-'./outPACKTEST/'
+
+  if(!dir.exists(outpath)){
+    dir.create(outpath)
+  }
+
+
+  gffpath<-'/home/mariost/DBs/ensembl/Homo_sapiens.GRCh37.87.gff3.gz'
+  reference<-'/home/mariost/DBs/references/Homo_sapiens/GATK/GRCh37/Sequence/WholeGenomeFasta/human_g1k_v37_decoy.fasta'
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # HGNC reference transcripts
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  hgnc<-read.delim(HGNC,header = T)
+  hgnc$ENselect<-str_match(hgnc$mane_select,'(ENST.+)\\|.+')[,2]
+  hgnc$ENselect <- gsub('\\..+','',hgnc$ENselect)
+
+
+
+
+  indir<-gsub('\\/\\/','/',vcfdir)
+  bamdir<-gsub('\\/\\/','/',bamdir)
+
+  sni<-str_match(indir,'.+/(.+)\\..+vcf.gz$')[,2]
+  bmi<-str_match(bamdir,'.+/(.+)\\..+bam$')[,2]
+  samples<-data.frame(Sample=sni,VCF=indir,BAM=bamdir[match(bmi,sni)])
+
+
+
+
+  mclapply(c(1:nrow(samples)),function(s){
+
+    VCF<-samples$VCF[s]
+    BAM<-samples$BAM[s]
+
+    OUTPUT<-gsub('.vcf.gz$','.WH.vcf.gz',VCF)
+    # vcfn<-VCF
+    vcfn<-gsub('.+/','',VCF)
+
+    OUTPUT<-paste0(outpath,gsub('.vcf.gz$','.WH.vcf.gz',vcfn))
+    OUTtxt<-paste0(outpath,gsub('.vcf.gz$','.WHcGT.txt',vcfn))
+    OUTstat<-paste0(outpath,gsub('.vcf.gz$','.WHstats.txt',vcfn))
+
+    args<-c('--distrust-genotypes --include-homozygous',paste('--changed-genotype-list',OUTtxt))
+    #
+    WHphase(vcf=VCF,bam = BAM,ref=reference, out=OUTPUT,args=args,whpath = WHpath)
+    #
+    system(paste0('tabix -f -p vcf ',OUTPUT))
+    #
+    WHstat(vcf = OUTPUT,whpath = WHpath,out=OUTstat)
+
+    OUTPUTpass<-gsub('.WH.vcf.gz$','.WHPASS.vcf.gz',OUTPUT)
+
+    OUTPUTcsqi<-gsub('.WH.vcf.gz$','.WHcsq_UN.vcf.gz',OUTPUT)
+
+    system(paste0("bcftools view -O z -o ",OUTPUTpass," -f PASS ",OUTPUT))
+
+    args<-c('--phase a --local-csq ')
+
+    BCFcsq(vcf = OUTPUTpass,ref = reference,out = OUTPUTcsqi,args = args,bcftpath = BCFtpath,gff = gffpath)
+    system(paste0('tabix -f -p vcf ',OUTPUTcsqi))
+
+    OUTPUTcsqi<-gsub('.WH.vcf.gz$','.WHcsq.vcf.gz',OUTPUT)
+    args<-c('--phase a')
+
+    BCFcsq(vcf = OUTPUTpass,ref = reference,out = OUTPUTcsqi,args = args,bcftpath = BCFtpath,gff = gffpath)
+
+    system(paste0('tabix -f -p vcf ',OUTPUTcsqi))
+
+    CSQfilter(OUTPUTcsq = OUTPUTcsqi,HGNC='/home/konstantinosp/tools/hgnc_complete_set.txt',OUTpath = outpath)
+
+  },mc.cores = ifelse(nrow(samples)<=ceiling(0.9*parallel::detectCores()),nrow(samples),ceiling(0.9*parallel::detectCores())))
+
+}
